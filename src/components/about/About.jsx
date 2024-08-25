4ff50f14db43ca5d534ba1559c8df419ba9979db
polo <privera.madrid@gmail.com>
2024-08-25 17:23:29 +0200

    Actualiza CV y modifica About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './about.css'
 import AboutImg from './../../assets/about.jpg'
-import CV from './../../assets/CV - José M. Polo.pdf'
+import CV from './../../assets/CV Jose Manuel Polo.pdf'
 import Info from "./Info";
 
 const About = () => {
